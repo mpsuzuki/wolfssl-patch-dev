f06ba65f2c78cbeef077d0cc529c293458a589e5
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2024-08-28 16:58:55 +0900

    [tests/xsleep.c] a substitution for the environment whose "sleep" is POSIX strict.
    * configure.ac: testing whether "sleep" accepts a floating number. if not, build xsleep.
    * tests/xsleep.c: a program accepting a floating number, aslike modern extended "sleep".
    * tests/include.am: add "xsleep" target with an appropriate conditionals.

---
+++ tests/xsleep.c
@@ -0,0 +1,46 @@
+/*
+ * POSIX does not request sleep(1) to support float argument,
+ * thus "sleep 0.1" is not guaranteed to be portable.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+  double      dsec = 0;
+  unsigned    isec = 0;
+  useconds_t  usec = 0;
+  char*       endp = NULL;
+
+  if (argc < 2) {
+    fprintf(stderr, "%s requires one argument\n", argv[0]);
+    exit(-1);
+  }
+
+  dsec = strtod(argv[1], &endp);
+  if (endp == NULL || endp[0]) {
+    fprintf(stderr, "Failed to parse %s as a floating number\n", argv[1]);
+    exit(-2);
+  }
+
+  if (dsec < 0) {
+    fprintf(stderr, "Negative value %f is unacceptable\n", dsec);
+    exit(-3);
+  }
+
+  if (UINT_MAX < dsec) {
+    fprintf(stderr, "Too big value %f is unacceptable\n", dsec);
+    exit(-4);
+  }
+
+  isec = (unsigned int)floor(dsec);
+  usec = (useconds_t)(int)((dsec - isec) * 1000000);
+
+  sleep(isec);
+  usleep(usec);
+
+  exit(0);
+}
